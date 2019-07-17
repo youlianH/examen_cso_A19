ab6e45ba9b9e7031fc463e9e722b7e3d27e60a72
youlianH <youlian@live.ca>
2019-07-17 04:19:38 +0000

    examen

--- LED.sh
+++ LED.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-# Initialisation des GPIOs. Les GPIOs nécessaires sont activés avec la commande export, puis la direction est choisie.
-echo "71" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio71/direction
 
-echo "233" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio233/direction
+sudo -i;
 
-while : 
+if [ ! -d "/sys/class/gpio/gpio71" ];
+	then
+	sudo echo "71" > /sys/class/gpio/export
+	sudo echo "out" > /sys/class/gpio/gpio71/direction
+	fi
+
+if [ ! -d "/sys/class/gpio/gpio233" ];
+	then
+	sudo echo "233" > /sys/class/gpio/export
+	sudo echo "out" > /sys/class/gpio/gpio233/direction
+	fi
+while :
 do
 if [ $(cat /sys/class/gpio/gpio233/value) -eq 0 ];
-	then 
+	then
 		echo "1" > /sys/class/gpio/gpio71/value
 	else
 		echo "0" > /sys/class/gpio/gpio71/value
-fi
+	fi
 done
